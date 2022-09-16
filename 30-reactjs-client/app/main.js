b918164c79a1857c183c04152c2765bbcadda201
Akshay Vinod <akshay.punnath@sap.com>
2022-09-17 01:43:51 +0530

    30-reactjs-client

---
+++ 30-reactjs-client/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const React = require("react");
+const ReactDOM = require("react-dom");
+import AddEmployee from "../components/add-employee";
+ReactDOM.render(<AddEmployee />, document.getElementById("react"));
